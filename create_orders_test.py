9f3872364c428264577cbe641d318eff0b3acbaf
Olga-9 <obelova1999@yandex.ru>
2024-06-18 22:17:13 +0500

    скриншот с запуском теста в PyCharm

--- create_orders_test.py
+++ create_orders_test.py
@@ -6,7 +6,10 @@ def get_order_track():
 
     return response.json()['track']
 
+
 def test_order_data_by_order_track():
     track = get_order_track()
     response = sender_stand_request.get_new_orders_track(track)
     assert response.status_code == 200
+
+# Ольга Белова, 17-я когорта — Финальный проект. Инженер по тестированию плюс
